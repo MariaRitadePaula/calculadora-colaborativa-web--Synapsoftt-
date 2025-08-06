286bdcd0023e0265e56f005f1410a8ec8ee69019
MariaRitadePaula <0001148949@senaimgaluno.com.br>
2025-08-06 14:07:09 -0300

    Update main.cs

--- Calculadora/main.cs
+++ Calculadora/main.cs
@@ -1,7 +1,36 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+        }
+
+       
+        static double Adicao(double a, double b)
+        {
+            return 0; 
+        }
+
+        static double Subtracao(double a, double b)
+        {
+            return 0; 
+        }
+
+        static double Multiplicacao(double a, double b)
+        {
+            return 0; 
+        }
+
+        static double Divisao(double a, double b)
+        {
+            return 0; 
+        }
+    }
