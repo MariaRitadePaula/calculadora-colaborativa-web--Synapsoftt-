b1b3f0ee5a8f69910ccb6587bc8376ae9913da27
MariaRitadePaula <0001148949@senaimgaluno.com.br>
2025-08-06 16:21:35 -0300

    Update main.cs

--- Calculadora/main.cs
+++ Calculadora/main.cs
@@ -2,35 +2,51 @@ using System;
 
     class Program
     {
+      static float a = 0;
+        static float b =0;
+        static float resultado =0;
+        
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Digite seu primeiro valor:");
+            a = float.Parse(Console.ReadLine());  
+            
             Console.WriteLine("Escolha a operação:");
             Console.WriteLine("1 - Adição");
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Multiplicação");
             Console.WriteLine("4 - Divisão");
+            int operação = int.Parse(Console.ReadLine()):
 
+            Console.WriteLine("Digite seu primeiro valor:");
+            b = float.Parse(Console.ReadLine());
         }
 
-       
-        static double Adicao(double a, double b)
+        if(operação == 1)
+            {
+            Adição();
+            Console.WriteLine(resultado);
+            }
+        else if(operação == 2)
         {
-            return 0; 
+            menos();
+            Console.WriteLine(resultado);
         }
-
-        static double Subtracao(double a, double b)
+        else if()
+        static void Adição();
         {
-            return 0; 
         }
-
-        static double Multiplicacao(double a, double b)
+        static void menos();
         {
-            return 0; 
         }
-
-        static double Divisao(double a, double b)
+        static void divisão();
         {
-            return 0; 
         }
+        static void multiplicação();
+        {
+        }
+        static void porcentagem();
+        {
+        }
+        
     }
